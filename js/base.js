5ad96052acce248dcb338f9cdfcb94ebfe7f0077
Vishnu Maurya <mauryavishnu98@gmail.com>
2020-07-20 15:28:10 +0530

    TC landing page

---
+++ js/base.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//   $(".imgbox").hover(
+//     function() {
+//       $(this).addClass("hovereffect")
+//     }
+//   )
+// })
+
+// $(document).ready(function () {
+//     $("#inside_img").hover(
+//         function({
+//             $(this.)
+//         })
+//     )
+// });
